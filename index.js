d0cb1c5bf9693bf979485a892ec66f0e404d873f
PatrikTorn <36461272+PatrikTorn@users.noreply.github.com>
2018-09-02 23:43:26 +0300

    a

---
+++ index.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var cors = require('cors');
+app.use(cors);
+server.listen(process.env.PORT || 5000);
+// WARNING: app.listen(80) will NOT work here!
+
+const connections = [];
+const playlist = [];
+
+io.on('connection', (socket) => {
+
+  socket.emit('get playlist', playlist);
+  io.sockets.emit('get connections', connections.length);
+  connections.push(socket);
+  console.log('connected', connections.length);
+
+  socket.on('disconnect', () => {
+    connections.splice(connections.indexOf(socket));
+    console.log('disconnected', connections.length);
+  });
+
+  socket.on('add song', (song) => {
+    playlist.push(song);
+    io.sockets.emit('get playlist', playlist);
+  });
+
+  socket.on('sort songs', (playlist) => {
+    playlist = playlist;
+    io.sockets.emit('get playlist', playlist);
+  });
+
+  socket.on('delete song', (songId) => {
+    playlist.splice(playlist.findIndex(pl => pl.id === songId), 1);
+    io.sockets.emit('get playlist', playlist);
+  });
+
+});
