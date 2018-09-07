6c015eeea65abdcb39c5f82df0945e65ee2f0e16
PatrikTorn <36461272+PatrikTorn@users.noreply.github.com>
2018-09-07 23:02:23 +0300

    uuid and room

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var cors = require('cors');
 var fs = require('file-system');
+var uuid = require('uuid/v1');
 app.use(cors);
 server.listen(process.env.PORT || 5000);
 
@@ -10,56 +11,59 @@ connections = [];
 rooms = [];
 
 io.on('connection', (socket) => {
+  socket.name = uuid();
+  socket.join('lobby');
   socket.emit('get rooms', rooms);
-  //io.sockets.emit('get connections', connections.length);
-
 
   socket.on('disconnect', () => {
-    connections.splice(connections.findIndex(c => c.socket === socket), 1);
-    // io.sockets.emit('get rooms', rooms.map(room => ({...room, connections: room.room === socket.room ? connections.filter(c => c.room === socket.room).length : room.connections})));
-    io.sockets.in(socket.room).emit('get connections', connections.filter(c => c.room === socket.room).length);
-    console.log('disconnected', connections.length);
+    rooms = rooms.map(r => r.room === socket.room ? {...r, connections:r.connections.filter(c => c !== socket.name)} : r);
+    socket.room && io.sockets.in(socket.room).emit('get connections', rooms.find(r => r.room === socket.room).connections);
+    console.log('disconnected', rooms.map(r => r.connections));
+    io.sockets.in('lobby').emit('get rooms', rooms);
   });
 
   socket.on('add song', (song) => {
     room = rooms.find(r => r.room === socket.room);
     room.playlist.push(song);
     io.sockets.in(room.room).emit('get playlist', room.playlist);
+    io.sockets.emit('get rooms', rooms);
     console.log(rooms);
   });
 
   socket.on('join room', (room) => {
+    socket.leave('lobby');
     socket.join(room);
     socket.room = room;
-    connections.push({socket, room});
-    io.sockets.in(room).emit('get connections', connections.filter(c => c.room === room).length);
-    // socket.emit('join room', room);
+    rooms = rooms.map(r => r.room === room ? {...r, connections:[...r.connections, socket.name].filter((a,i,s) => s.indexOf(a) === i)} : r);
+    io.sockets.in(room).emit('get connections', rooms.find(r => r.room === room).connections);
     socket.emit('get playlist', rooms.find(r => r.room === room).playlist);
-    console.log('join room and get playlist');
+    io.sockets.in('lobby').emit('get rooms', rooms);
+    console.log('join room', rooms.map(r => r.connections));
   });
 
   socket.on('exit room', (room) => {
     socket.leave(room);
     socket.room = null;
-    connections.splice(connections.findIndex(c => c.socket === socket), 1);
-    io.sockets.in(room).emit('get connections', connections.filter(c => c.room === room).length);
-    // socket.emit('exit room');
-    // socket.emit('get playlist', []);
+    rooms = rooms.map(r => r.room === room ? {...r, connections:r.connections.filter(c => c !== socket.name)} : r);
+    io.sockets.in(room).emit('get connections', rooms.find(r => r.room === room).connections);
+    io.sockets.in('lobby').emit('get rooms', rooms);
+    console.log(rooms.map(r => r.connections));
   });
 
   socket.on('create room', (room) => {
+    socket.leave('lobby');
     socket.join(room);
     socket.room = room;
     rooms.push({
       room,
-      playlist:[]
+      playlist:[],
+      connections:[socket.name]
     });
-    connections.push({socket, room});
-    io.sockets.in(room).emit('get connections', connections.filter(c => c.room === room).length);
-    io.sockets.emit('get rooms', rooms);
-    socket.emit('join room', room);
-    console.log('connected', connections.filter(c => c.room === room).length);
-    console.log('create room', rooms);
+    // io.sockets.in(room).emit('get connections', rooms.find(r => r.room === room).connections);
+    io.sockets.in('lobby').emit('get rooms', rooms);
+    // socket.emit('join room', room);
+    io.sockets.in(room).emit('get connections', rooms.find(r => r.room === room).connections);
+    console.log('create room', rooms.map(r => r.connections));
   });
 
   socket.on('sort songs', (list) => {
